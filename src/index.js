c2f04dc7a5348920b50ebde801c7d6773f59c0fa
Shordal <oneholy@hotmail.com>
2020-01-15 09:35:05 -0600

    refactor, bring out notes from main div

--- src/index.js
+++ src/index.js
@@ -1,33 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import OTSBanner from './images/OTSBanner.svg';
 
-const NoteApp = () => {
-    document.title = 'Lone Star'
-    const notesData = JSON.parse(localStorage.getItem('notes'))
-    const [notes, setNotes] = useState(notesData || [])
+const notesReducer = (state, action) => {
+        switch (action.type) {
+            case 'POPULATE_NOTES':
+                return action.notes
+            case 'ADD_NOTE':
+                return [
+                    // coppies the exsisting objects to the array
+                    ...state,
+                    // below adds an object with the title and body of a note
+                    { title: action.title, body: action.body }
+                ]
+            case 'REMOVE_NOTE':
+                return state.filter((note) => note.title !== action.title )
+            default:
+                return state
+        }
+    }
+
+const QuickMeeting = () => {
+    document.title = 'Lone Star Quick Meeting'
+        //const [notes, setNotes] = useState([])
+        // below is more complex useState
+        const [notes, dispatch] = useReducer(notesReducer, [])
+        // simple State use
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
 
     const addNote = (e) => {
         e.preventDefault()
-        setNotes([
-            ...notes,
-            { title, body }
-        ])
+        dispatch({
+                        type: 'ADD_NOTE',
+                        title,
+                        body
+                    })
+                    // setNotes([
+                    //     ...notes,
+                    //     { 
+                    //     title,
+                    //     body 
+                    //     }
+                    // ])
         setTitle('')
         setBody('')
     }
 
-    const removeNote = (title) => {
-        setNotes(notes.filter((note) => note.title !== title))
-    }
+        // fetching data from a database
+    useEffect(() => {
+        console.log('Fetching local data')
+        const notes = JSON.parse(localStorage.getItem('notes'))
+        if (notes) {
+            dispatch({ type: 'POPULATE_NOTES', notes })
+            // setNotes(notesData)
+        }
+    }, [])
+
+    const removeNote = (title) => {
+            // setNotes(notes.filter((note) => note.title !== title))
+            dispatch({
+                type: 'REMOVE_NOTE',
+                title
+            })
+        }
 
     useEffect(() => {
         localStorage.setItem('notes', JSON.stringify(notes))
-    })
+    }, [notes])
 
     return (
         <div className="NoteApp">
@@ -35,11 +77,7 @@ const NoteApp = () => {
             {/* <h1 className="NoteApp-header">Lone Star College</h1>
             <h1 className="NoteApp-header">Tomball</h1> */}
             {notes.map((note) => (
-                <div className="NoteApp-note" key={note.title}>
-                    <h3>{note.title}</h3>
-                    <p>{note.body}</p>
-                    <button className="Button-body" onClick={() => removeNote(note.title)}>Remove</button>
-                </div>
+                <Note key={note.title} note={note} removeNote={removeNote}/>
             ))}
             <div className="NoteApp-inputheader"> 
             <p>Add Content</p>
@@ -53,27 +91,23 @@ const NoteApp = () => {
     )
 }
 
-const App = (props) => {
-    const [count, setCount] = useState(props.count)
-    const [text, setText] = useState('')
+const Note = ({ note, removeNote }) => {
+        useEffect(() => {
+            console.log('Setting up effect!')
+            return () => {
+                console.log('Cleaning up effect!')
+            }
+        }, [])
+        return (
+            <div className="NoteApp-note">
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button className="Button-body" onClick={() => removeNote(note.title)}>X</button>
+        </div>
+        )
+    }
 
-    useEffect(() => {
-        console.log('useEffect ran')
-        document.title = count
-    })
-
-    return (
-        <div>
-            <p>The current {text || 'count'} is {count}</p>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-            <button onClick={() => setCount(props.count)}>reset</button>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-            <input value={text} onChange={(e) => setText(e.target.value)}/>
-        </div>
-    )
-}
-
-ReactDOM.render(<NoteApp/>, document.getElementById('root'));
+ReactDOM.render(<QuickMeeting />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
