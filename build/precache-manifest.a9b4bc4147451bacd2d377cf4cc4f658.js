4ceb6684be6888eba6c25b9bc9dc46b64d56c7ae
Shordal <oneholy@hotmail.com>
2020-01-15 10:40:34 -0600

    added build

---
+++ build/precache-manifest.a9b4bc4147451bacd2d377cf4cc4f658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726e81c4109789201068abcb25035bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca6e099086db802b722",
+    "url": "/static/css/main.8a2b43c1.chunk.css"
+  },
+  {
+    "revision": "e2be31e236d8fde50ce6",
+    "url": "/static/js/2.eeb112e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eeb112e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ca6e099086db802b722",
+    "url": "/static/js/main.6a87b3b6.chunk.js"
+  },
+  {
+    "revision": "100fc765607beee54604",
+    "url": "/static/js/runtime-main.0a6132a1.js"
+  },
+  {
+    "revision": "e1ff66e195f72a23ed8749fa0b17f2cf",
+    "url": "/static/media/OTSBanner.e1ff66e1.svg"
+  }
+]);
