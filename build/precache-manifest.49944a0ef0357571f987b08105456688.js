dde26b697a828811e42a8fa0744b6ec2087e43cb
Shordal <oneholy@hotmail.com>
2020-01-15 13:11:29 -0600

    corrected homePage

---
+++ build/precache-manifest.49944a0ef0357571f987b08105456688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e004ce447485cb8668db6877b4ffd042",
+    "url": "/QuickMeeting/index.html"
+  },
+  {
+    "revision": "aec54da2e39e785122f3",
+    "url": "/QuickMeeting/static/css/main.8a2b43c1.chunk.css"
+  },
+  {
+    "revision": "e2be31e236d8fde50ce6",
+    "url": "/QuickMeeting/static/js/2.eeb112e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/QuickMeeting/static/js/2.eeb112e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aec54da2e39e785122f3",
+    "url": "/QuickMeeting/static/js/main.886263da.chunk.js"
+  },
+  {
+    "revision": "ad0a316095b66b9f0551",
+    "url": "/QuickMeeting/static/js/runtime-main.2dc85dfe.js"
+  },
+  {
+    "revision": "e1ff66e195f72a23ed8749fa0b17f2cf",
+    "url": "/QuickMeeting/static/media/OTSBanner.e1ff66e1.svg"
+  }
+]);
